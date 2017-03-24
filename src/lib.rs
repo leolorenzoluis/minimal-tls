0c8dd1a9b580557a65931019a99ef883705056af
Cyrus Malekpour <cmalekpour@gmail.com>
2017-03-24 16:19:13 -0400

    Moved some stuff around in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,24 @@
 mod structures;
 
-mod minimaltls {
-	use structures::ContentType;
+use std::io::Read;
+use std::io::Write;
 
-	fn main() {
-		let x : ContentType = ContentType::Alert;
-	}
+use structures::ContentType;
+
+pub struct TLS_config<'a> {
+	reader : &'a Read,
+	writer : &'a Write,
 }
+
+fn TLS_init<'a, R : Read, W : Write>(read : &'a R, write : &'a W) -> TLS_config<'a> {
+	TLS_config{reader : read, writer : write}
+}
+
+// Ideas for functions...
+// TLS_start -> handshake and connection setup
+// TLS_send -> sends plaintext
+// TLS_recieve -> recieves plaintext
+// TLS_end -> closes the connection
 
 #[cfg(test)]
 mod tests {
