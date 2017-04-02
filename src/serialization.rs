56e17290cfd23f8e319835c2569e9c4869847a9e
Cyrus Malekpour <cmalekpour@gmail.com>
2017-04-01 20:44:41 -0400

    Fleshed out some more serialization code

--- src/serialization.rs
+++ src/serialization.rs
@@ -7,6 +7,30 @@ pub trait TLSToBytes {
 	fn as_bytes(&self) -> Vec<u8>;
 }
 
+/*
+	We can't use a generic Vec<T> implementation for these two functions
+	because TLS 1.3 encodes length differently depending on the max length
+	of the vector. If the cap is <= 255, it only uses one byte, but otherwise
+	uses two bytes. I think this is really silly but oh well
+*/
+fn u16_vector_as_bytes<T>(data : &Vec<T>) -> Vec<u8> where T:TLSToBytes {
+	let mut ret : Vec<u8> = vec![];
+	ret.write_u16::<NetworkEndian>(data.len() as u16).unwrap();
+	for x in data.iter() {
+		ret.extend(x.as_bytes());
+	}
+	ret
+}
+
+fn u8_vector_as_bytes<T>(data : &Vec<T>) -> Vec<u8> where T:TLSToBytes {
+	let mut ret : Vec<u8> = vec![];
+	ret.push(data.len() as u8);
+	for x in data.iter() {
+		ret.extend(x.as_bytes());
+	}
+	ret
+}
+
 // FIXME: These need to be implemented
 impl<'a> TLSToBytes for &'a TLSPlaintext {
 	fn as_bytes(&self) -> Vec<u8> {
@@ -30,6 +54,19 @@ impl<'a> TLSToBytes for &'a TLSPlaintext {
 
 impl<'a> TLSToBytes for &'a HandshakeMessage {
 	fn as_bytes(&self) -> Vec<u8> {
-		Vec::new()
+	    match **self {
+			HandshakeMessage::InvalidMessage => vec![],
+			HandshakeMessage::ClientHello(ref inner) => vec![],
+			HandshakeMessage::ServerHello(ref inner) => vec![],
+			HandshakeMessage::EndOfEarlyData(ref inner) => vec![],
+			HandshakeMessage::HelloRetryRequest(ref inner) => vec![],
+			HandshakeMessage::EncryptedExtensions(ref inner) => vec![],
+			HandshakeMessage::CertificateRequest(ref inner) => vec![],
+			HandshakeMessage::Certificate(ref inner) => vec![],
+			HandshakeMessage::CertificateVerify(ref inner) => vec![],
+			HandshakeMessage::Finished(ref inner) => vec![],
+			HandshakeMessage::NewSessionTicket(ref inner) => vec![],
+			HandshakeMessage::KeyUpdate(ref inner) => vec![],
+	    }
 	}
 }
